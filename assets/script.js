fcabbdbe02490798c88249b681787b833c795503
Lee-moran <leemoran06@hotmail.com>
2022-02-09 14:55:12 +0000

    question function and btn

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,23 @@ function startGame() {
     setNextQuestion();
 
 }
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
+
+}
